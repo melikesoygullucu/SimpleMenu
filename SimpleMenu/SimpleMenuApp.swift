90bdbceb2a0d208a10a419d7fec2ff9514186c60
melikesoygullucu <melikesoygul@hotmail.com>
2024-06-12 14:11:06 +0300

    Initial Commit

---
+++ SimpleMenu/SimpleMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMenuApp.swift
+//  SimpleMenu
+//
+//  Created by Melike Soygüllücü on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
