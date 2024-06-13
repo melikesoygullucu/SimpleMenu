76bd116146c7d2fd7b87dddcc1bb74b488be14fe
melikesoygullucu <melikesoygul@hotmail.com>
2024-06-13 13:17:47 +0300

    meal restrictions added

--- SimpleMenu/ContentView.swift
+++ SimpleMenu/ContentView.swift
@@ -31,13 +31,29 @@ struct ContentView: View {
 
 struct ItemRow: View {
     let item: MenuItem
-    
+    let colors: [String: Color] = [ "D": .purple , "G": .black, "N": .red, "S": .blue, "V": .green]
+    // G stands for Gluten, V stands for Vegeterian and so on.
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(.gray, lineWidth: 2))
             VStack(alignment: .leading){
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
+                    
+            }
+            Spacer()
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundStyle(.white)
+                
             }
         }
     }
