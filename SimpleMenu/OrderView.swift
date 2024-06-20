14ae0308adf6d58d2f90cc4fc57bbb518f5c0f8c
melikesoygullucu <melikesoygul@hotmail.com>
2024-06-20 18:58:39 +0300

    order button and function added

---
+++ SimpleMenu/OrderView.swift
@@ -0,0 +1,38 @@
+//
+//  OrderView.swift
+//  SimpleMenu
+//
+//  Created by Melike Soygüllücü on 20.06.2024.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    var body: some View {
+        NavigationStack{
+            List{
+                Section{
+                    ForEach(order.items) { item in
+                        HStack{
+                            Text(item.name)
+                            Spacer()
+                            Text("Price: \(item.price)")
+                        }
+                    }
+                }
+                Section{
+                    NavigationLink("Place Order"){
+                        Text("Check Out")
+                    }
+                }
+            }
+            .navigationTitle("Order")
+        }
+    }
+}
+
+#Preview {
+    OrderView()
+        .environmentObject(Order())
+}
