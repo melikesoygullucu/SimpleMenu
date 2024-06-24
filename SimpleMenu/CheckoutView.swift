fabe5c630307ec8e48633e4bc8e1e10432a53eae
melikesoygullucu <melikesoygul@hotmail.com>
2024-06-24 11:18:18 +0300

    checkout view added

---
+++ SimpleMenu/CheckoutView.swift
@@ -0,0 +1,35 @@
+//
+//  CheckoutView.swift
+//  SimpleMenu
+//
+//  Created by Melike Soygüllücü on 24.06.2024.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    @State private var paymentType = "Cash"
+    
+    let paymentTypes = ["Cash", "CreditCard", "IDine Points"]
+    
+    
+    var body: some View {
+        VStack {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(paymentTypes, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    CheckoutView()
+        .environmentObject(Order())
+}
